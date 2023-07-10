1c4986da05a386fc2c67e51afa2297d24a066c4d
Yaroslav <yaroslav.honcharenko.ua@gmail.com>
2023-07-10 11:42:16 +0300

    model Song

--- Songs/models.py
+++ Songs/models.py
@@ -1,3 +1,20 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Songs(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200, unique=True)
+    content = models.TextField(max_length=600, null=True, blank=True)
+    category = models.CharField(max_length=100, null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "songs"
+        ordering = ['-createdAt']
+        verbose_name = "Song"
+        verbose_name_plural = "Songs"
+
+    def __str__(self) -> str:
+        return self.title
