9c1d94e1c731b2fc9881e9e91bbbba1a251e531e
Yaroslav <yaroslav.honcharenko.ua@gmail.com>
2023-07-10 11:48:09 +0300

    admin Songs

--- Songs/admin.py
+++ Songs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from Songs.models import Songs
 
-# Register your models here.
+
+class SongsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'category', 'createdAt', 'updatedAt')
+    list_filter = ('createdAt', 'updatedAt')
+    search_fields = ('title',)
+
+
+admin.site.register(Songs, SongsAdmin)
