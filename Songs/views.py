df6dd084ecb9b5e9bd90d5b71b777fe22be98a24
Yaroslav <yaroslav.honcharenko.ua@gmail.com>
2023-07-10 11:45:59 +0300

    view for Song

--- Songs/views.py
+++ Songs/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
+from Songs.models import Songs
+from Songs.serializers import SongsSerializer
 
-# Create your views here.
+
+class SongsListPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+
+class SongsListCreateView(generics.ListCreateAPIView):
+    queryset = Songs.objects.all()
+    serializer_class = SongsSerializer
+    pagination_class = SongsListPagination
+
+
+class SongsRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Songs.objects.all()
+    serializer_class = SongsSerializer
