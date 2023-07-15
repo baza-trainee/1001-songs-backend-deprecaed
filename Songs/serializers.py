b20e33e02ae625b620645008b76e1cc9894c266c
Yaroslav <yaroslav.honcharenko.ua@gmail.com>
2023-07-15 11:35:58 +0300

    added serializer

--- Songs/serializers.py
+++ Songs/serializers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
-from Songs.models import Song
+from Songs.models import Song, SongLocation, SongDetails, SongMedia
 
 
-class SongsSerializer(serializers.ModelSerializer):
+class SongLocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SongLocation
+        fields = '__all__'
+
+
+class SongDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SongDetails
+        fields = '__all__'
+
+
+class SongMediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SongMedia
+        fields = '__all__'
+
+
+class SongSerializer(serializers.ModelSerializer):
+    location = SongLocationSerializer()
+    details = SongDetailsSerializer()
+    media = SongMediaSerializer()
+
     class Meta:
         model = Song
         fields = '__all__'
