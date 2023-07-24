7bc944aef434ccb92414e009475d64aa75dfc555
vvaldd <wroztorguev@gmail.com>
2023-07-24 22:48:41 +0300

    test map

--- apps/staticmap/views.py
+++ apps/staticmap/views.py
@@ -2,14 +2,10 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from apps.songs.models import Song
-from apps.songs.serializers import SongSerializer
 
 
-# class MapListView(GenericAPIView):
-#     def get(self, *args, **kwargs):
-#         qs = Song.objects.select_related('region').count()
-#         list1 = [a for a in qs.o]
-#         serializer = SongSerializer(instance=qs, many=True)
-#         print(list1)
-#
-#         return Response(qs)
+class MapListView(GenericAPIView):
+    def get(self, *args, **kwargs):
+        params = self.request.query_params.dict()
+        result_count = Song.objects.filter(location__region__in=params['region']).count()
+        return Response(result_count)
