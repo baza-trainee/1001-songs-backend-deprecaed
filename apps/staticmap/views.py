dba80036d01a58c009e5ba320b76a9bf23c4245c
vvaldd <wroztorguev@gmail.com>
2023-07-26 23:00:09 +0300

    test map

--- apps/staticmap/views.py
+++ apps/staticmap/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
@@ -7,5 +8,9 @@ from apps.songs.models import Song
 class MapListView(GenericAPIView):
     def get(self, *args, **kwargs):
         params = self.request.query_params.dict()
-        result_count = Song.objects.filter(location__region__in=params['region']).count()
-        return Response(result_count)
+        if params:
+            result_count = Song.objects.all().filter(location__region=params['region']).count()
+            return Response(result_count)
+        else:
+            result_count = Song.objects.all().values('location__region').annotate(Count('location__region'))
+            return Response(result_count)
