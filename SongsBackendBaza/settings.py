a9ae8d7dd6dece906f3abb2eb434577b74427960
Ярослав <yaroslavhoncharenko1@gmail.com>
2023-07-29 22:12:27 +0300

    search, sort, ordering

--- SongsBackendBaza/settings.py
+++ SongsBackendBaza/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django_otp',
     'django_otp.plugins.otp_totp',
     'django_otp.plugins.otp_static',
+    'django_filters',
 
     'apps.auth',
     'apps.users',
--- apps/songs/views.py
+++ apps/songs/views.py
@@ -1,12 +1,25 @@
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from apps.songs.models import Song
 from apps.songs.serializers import SongSerializer
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class SongListCreateView(ListCreateAPIView):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = [
+        'location__country',
+        'location__region',
+        'location__ethnographic_district',
+        'details__genre_cycle'
+    ]
+    search_fields = ['title']
+    ordering_fields = ['title', 'recording_date']
+    ordering = ['title']
 
 
 class SongRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
