c8a3a0ff92a24f5ae44865ba3fa5b195f149f995
vvaldd <wroztorguev@gmail.com>
2023-08-06 16:11:02 +0300

    test 1

--- SongsBackendBaza/settings.py
+++ SongsBackendBaza/settings.py
@@ -16,9 +16,7 @@ import dj_database_url
 
 from .extra_conf import *
 
-# env = environ.Env()
-#
-# environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
