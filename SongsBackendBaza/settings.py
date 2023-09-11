6891448491be54258201a79c2e3e4d1ab2914ad0
vvaldd <wroztorguev@gmail.com>
2023-09-11 18:28:51 +0300

    req + settings + env

--- SongsBackendBaza/settings.py
+++ SongsBackendBaza/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-@3(0py92b@kua^!!4=bemp&(4yrhi8v3yo=i732(+q76=cbdco'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
