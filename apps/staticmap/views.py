c5248ded55693b0b15750790935b975377721f30
vvaldd <wroztorguev@gmail.com>
2023-09-14 00:48:56 +0300

    edit songmodel, add map + region

--- apps/staticmap/views.py
+++ apps/staticmap/views.py
@@ -1,8 +1,10 @@
 from django.db.models import Count
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from apps.songs.models import Song
+from apps.songs.serializers import SongSerializer
 
 
 class MapListView(GenericAPIView):
@@ -10,3 +12,15 @@ class MapListView(GenericAPIView):
     def get(*args, **kwargs):
         result_count = Song.objects.all().values('location__region').annotate(Count('location__region'))
         return Response(result_count)
+
+
+class MapRegionListView(GenericAPIView):
+    def get(self, *args, **kwargs):
+        params = self.request.query_params.dict()
+        if params:
+            songs_region = Song.objects.filter(location__region=params['region'])
+            serializer = SongSerializer(instance=songs_region, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response('Specify the search region', status=status.HTTP_404_NOT_FOUND)
+
