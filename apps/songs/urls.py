3308d150992eb1be5baea395710bb1e68f34ae05
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-09-29 12:41:55 +0300

    Update urls.py
    new urls song location

--- apps/songs/urls.py
+++ apps/songs/urls.py
@@ -5,4 +5,5 @@ from .views import SongListCreateView, SongRetrieveUpdateDestroyView
 urlpatterns = [
     path('', SongListCreateView.as_view(), name='list_crate_song'),
     path('/<uuid:pk>', SongRetrieveUpdateDestroyView.as_view(), name='songs_retrieve_update_destroy_view'),
+    path('songs_by_location/', SongsByLocationView.as_view(), name='songs-by-location'),
 ]
