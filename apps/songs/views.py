1f6ebadbf32ae53b1d52149dd9cfd7f8a9954126
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-09-29 13:18:26 +0300

    Update views.py

--- apps/songs/views.py
+++ apps/songs/views.py
@@ -6,7 +6,7 @@ from apps.songs.serializers import SongSerializer
 
 from django_filters.rest_framework import DjangoFilterBackend
 
-from django.shortcuts import get_list_or_404
+from django.db.models import Q
 from rest_framework import generics, status
 from rest_framework.response import Response
 
@@ -35,28 +35,29 @@ class SongsByLocationView(generics.GenericAPIView):
     Retrieve a list of songs recorded in a specific location.
 
     Parameters:
-    - location: The country where the songs were recorded.
+    - location: The official city name where the songs were recorded.
 
     Usage Example:
-    GET /songs_by_location/?location=Ukraine
+    GET /songs_by_location/?location=Kyiv
     """
     serializer_class = SongSerializer
 
-    def get_queryset(self, location):
+    def get_queryset(self, official_name_city):
         try:
-            return get_list_or_404(Song, location__country=location)
+            return Song.objects.filter(Q(location__official_name_city__iexact=official_name_city) | Q(location__official_name_city__icontains=official_name_city))
         except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return None
 
     def get(self, request, *args, **kwargs):
         location = self.request.query_params.get('location', None)
         if not location:
             return Response({"error": "Location parameter is required"}, status=status.HTTP_400_BAD_REQUEST)
 
-        try:
-            queryset = self.get_queryset(location)
-        except Exception as e:
+        queryset = self.get_queryset(location)
+        if queryset is None:
             return Response({"error": "An error occurred while fetching data"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        elif not queryset.exists():
+            return Response({"error": "No data found for the specified location"}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
