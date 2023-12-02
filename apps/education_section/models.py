7978843f9f5b60a14c3845e3af58569621d09ad6
vvaldd <wroztorguev@gmail.com>
2023-12-02 19:31:18 +0200

    educational section

--- apps/education_section/models.py
+++ apps/education_section/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 class EducationalSection(models.Model):
+    class Meta:
+        db_table = "educational_sections"
+        ordering = ['-created_at']
+        verbose_name = "Educational Section"
+        verbose_name_plural = "Educational Sections"
+
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     photo = models.ImageField(blank=True)
