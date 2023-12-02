8be5a13478e07d447db4c634973c648eb54f2b04
vvaldd <wroztorguev@gmail.com>
2023-12-02 17:21:58 +0200

    marker filter

--- apps/staticmap/urls.py
+++ apps/staticmap/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from apps.staticmap.views import MapListView, MapRegionListView
+from apps.staticmap.views import MapListView, MapCityListView
 
 urlpatterns = [
     path('', MapListView.as_view(), name='list_map'),
-    path('/city', MapRegionListView.as_view(), name='region_list_map'),
+    path('/city', MapCityListView.as_view(), name='city_list_map'),
 ]
--- apps/staticmap/views.py
+++ apps/staticmap/views.py
@@ -9,46 +9,46 @@ from apps.songs.serializers import SongSerializer
 
 class MapListView(GenericAPIView):
     """
-    List of songs by regions
+    List of songs by city
     """
     @staticmethod
     def get(*args, **kwargs):
         # не работает на версии джанго под рендер
         result_count = (Song.objects.all().values('location__official_name_city', 'location__coordinates')
                         .annotate(count=Count('location__official_name_city')))
-        count_region = Song.objects.all().aggregate(
-            Рівне=Count('id', filter=Q(location__region='Рівне')),
-            Кіровоград=Count('id', filter=Q(location__region='Кіровоград')),
-            Полтава=Count('id', filter=Q(location__region='Полтава')),
-            Вінниця=Count('id', filter=Q(location__region='Вінниця')),
-            Луцьк=Count('id', filter=Q(location__region='Луцьк')),
-            Дніпро=Count('id', filter=Q(location__region='Дніпро')),
-            Донецк=Count('id', filter=Q(location__region='Донецк')),
-            Житомир=Count('id', filter=Q(location__region='Житомир')),
-            Ужгород=Count('id', filter=Q(location__region='Ужгород')),
-            Запоріжжя=Count('id', filter=Q(location__region='Запоріжжя')),
-            Івано_Франківськ=Count('id', filter=Q(location__region='Івано_Франківськ')),
-            Київ=Count('id', filter=Q(location__region='Київ')),
-            Луганськ=Count('id', filter=Q(location__region='Луганськ')),
-            Львів=Count('id', filter=Q(location__region='Львів')),
-            Миколаїв=Count('id', filter=Q(location__region='Миколаїв')),
-            Одеса=Count('id', filter=Q(location__region='Одеса')),
-            Суми=Count('id', filter=Q(location__region='Суми')),
-            Тернопіль=Count('id', filter=Q(location__region='Тернопіль')),
-            Харьків=Count('id', filter=Q(location__region='Харьків')),
-            Херсон=Count('id', filter=Q(location__region='Херсон')),
-            Хмельницький=Count('id', filter=Q(location__region='Хмельницький')),
-            Черкаси=Count('id', filter=Q(location__region='Черкаси')),
-            Чернівці=Count('id', filter=Q(location__region='Чернівці')),
-            Чернігів=Count('id', filter=Q(location__region='Чернігів')),
-            Крим=Count('id', filter=Q(location__region='Крим')),
-        )
+        # count_region = Song.objects.all().aggregate(
+        #     Рівне=Count('id', filter=Q(location__region='Рівне')),
+        #     Кіровоград=Count('id', filter=Q(location__region='Кіровоград')),
+        #     Полтава=Count('id', filter=Q(location__region='Полтава')),
+        #     Вінниця=Count('id', filter=Q(location__region='Вінниця')),
+        #     Луцьк=Count('id', filter=Q(location__region='Луцьк')),
+        #     Дніпро=Count('id', filter=Q(location__region='Дніпро')),
+        #     Донецк=Count('id', filter=Q(location__region='Донецк')),
+        #     Житомир=Count('id', filter=Q(location__region='Житомир')),
+        #     Ужгород=Count('id', filter=Q(location__region='Ужгород')),
+        #     Запоріжжя=Count('id', filter=Q(location__region='Запоріжжя')),
+        #     Івано_Франківськ=Count('id', filter=Q(location__region='Івано_Франківськ')),
+        #     Київ=Count('id', filter=Q(location__region='Київ')),
+        #     Луганськ=Count('id', filter=Q(location__region='Луганськ')),
+        #     Львів=Count('id', filter=Q(location__region='Львів')),
+        #     Миколаїв=Count('id', filter=Q(location__region='Миколаїв')),
+        #     Одеса=Count('id', filter=Q(location__region='Одеса')),
+        #     Суми=Count('id', filter=Q(location__region='Суми')),
+        #     Тернопіль=Count('id', filter=Q(location__region='Тернопіль')),
+        #     Харьків=Count('id', filter=Q(location__region='Харьків')),
+        #     Херсон=Count('id', filter=Q(location__region='Херсон')),
+        #     Хмельницький=Count('id', filter=Q(location__region='Хмельницький')),
+        #     Черкаси=Count('id', filter=Q(location__region='Черкаси')),
+        #     Чернівці=Count('id', filter=Q(location__region='Чернівці')),
+        #     Чернігів=Count('id', filter=Q(location__region='Чернігів')),
+        #     Крим=Count('id', filter=Q(location__region='Крим')),
+        # )
         return Response(result_count)
 
 
-class MapRegionListView(GenericAPIView):
+class MapCityListView(GenericAPIView):
     """
-    List of songs in the selected region
+    List of songs in the selected city
     """
     queryset = Song.objects.all()
 
