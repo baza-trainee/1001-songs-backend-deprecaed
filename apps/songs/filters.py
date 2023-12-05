9196de4246fc1da5cf5fcf1b883776ec97c8a976
vvaldd <wroztorguev@gmail.com>
2023-12-05 21:34:38 +0200

    filters

--- apps/songs/filters.py
+++ apps/songs/filters.py
@@ -7,12 +7,12 @@ class SongFilter(filters.FilterSet):
     class Meta:
         model = Song
         fields = (
-            'country', 'region', 'district', 'genre', 'title', 'archive',
+            'country', 'region', 'city', 'genre', 'title', 'archive',
         )
 
     country = filters.CharFilter(field_name='location__country', lookup_expr='icontains')
     region = filters.CharFilter(field_name='location__region', lookup_expr='icontains')
-    district = filters.CharFilter(field_name='location__district_center', lookup_expr='icontains')
+    city = filters.CharFilter(field_name='location__official_name_city', lookup_expr='icontains')
     genre = filters.CharFilter(field_name='details__genre_cycle', lookup_expr='icontains')
     title = filters.CharFilter(field_name='title', lookup_expr='icontains')
     archive = filters.CharFilter(field_name='archive', lookup_expr='icontains')
