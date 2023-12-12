b483cb7f76fb0040f9805f3ebaad91e7cff80125
vvaldd <wroztorguev@gmail.com>
2023-12-12 21:11:21 +0200

    filters clear

--- apps/songs/filters.py
+++ apps/songs/filters.py
@@ -7,11 +7,11 @@ class SongFilter(filters.FilterSet):
     class Meta:
         model = Song
         fields = (
-            'country', 'region', 'city', 'genre', 'title', 'archive',
+            'title', 'country', 'region', 'city', 'genre', 'archive',
         )
+    title = filters.CharFilter(field_name='title', lookup_expr='icontains')
     country = filters.BaseInFilter(field_name='location__country', lookup_expr='in')
     region = filters.BaseInFilter(field_name='location__region', lookup_expr='in')
     city = filters.BaseInFilter(field_name='location__official_name_city', lookup_expr='in')
     genre = filters.BaseInFilter(field_name='details__genre_cycle', lookup_expr='in')
-    title = filters.BaseInFilter(field_name='title', lookup_expr='in')
     archive = filters.BaseInFilter(field_name='archive', lookup_expr='in')
