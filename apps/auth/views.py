366a52e098da1152c66b946dcec42e6cf5ddd3ac
vvaldd <wroztorguev@gmail.com>
2024-01-18 20:35:48 +0200

    edit admin users

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,7 +1,7 @@
 from rest_framework.generics import CreateAPIView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-from apps.auth.serializers import CustomTokenObtainPairSerializer, CustomTokenRefreshSerializers
+from apps.auth.serializers import CustomTokenObtainPairSerializer
 from apps.users.serializers import UserSerializer
 
 
@@ -19,8 +19,8 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
 
-class CustomTokenRefreshView(TokenRefreshView):
-    """
-        Refresh
-    """
-    serializer_class = CustomTokenRefreshSerializers
+# class CustomTokenRefreshView(TokenRefreshView):
+#     """
+#         Refresh
+#     """
+#     serializer_class = CustomTokenRefreshSerializers
